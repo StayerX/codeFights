466e7b826abc786ae7bad415a3ed4b25049c2bd9
Stanislav Yotov <yotov.st@gmail.com>
2016-05-16 15:07:17 +0100

    working v1

--- FirstKindSum/first_kind_sum_it_up.py
+++ FirstKindSum/first_kind_sum_it_up.py
@@ -1,7 +1,7 @@
-
 import itertools, math, decimal
 
-def First_Kind_Sum_It_Up(n, k):
+
+def first_kind_sum_it_up(n, k):
     r = kbits(n,k, 100000)
     div = max(r)*1.0
     res = sum([div/x for x in r])/div
@@ -11,7 +11,8 @@ def First_Kind_Sum_It_Up(n, k):
     res = ('%sE+%d' % (re,pow)) if pow>=0 else ('%sE%d' % (re,pow))
     return res
 
+
 def kbits(n, k, l):
     bits = itertools.combinations(range(1,n+1), k)
-    bits = itertools.islice(bits,l)
+    # bits = itertools.islice(bits,l)
     return map(lambda bit: reduce(lambda x,y: x*y, bit),bits)
