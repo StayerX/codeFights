07a616c9809973465debb0cbaf1c74b2da521334
Stanislav Yotov <yotov.st@gmail.com>
2016-05-16 15:13:54 +0100

    v2

--- FirstKindSum/first_kind_sum_it_up.py
+++ FirstKindSum/first_kind_sum_it_up.py
@@ -1,8 +1,9 @@
 import itertools, math, decimal
 
+mult = {}
 
 def first_kind_sum_it_up(n, k):
-    r = kbits(n,k, 100000)
+    r = kbits(n,k,1)
     div = max(r)*1.0
     res = sum([div/x for x in r])/div
     d = decimal.Decimal(res).as_tuple()
@@ -11,8 +12,17 @@ def first_kind_sum_it_up(n, k):
     res = ('%sE+%d' % (re,pow)) if pow>=0 else ('%sE%d' % (re,pow))
     return res
 
-
 def kbits(n, k, l):
     bits = itertools.combinations(range(1,n+1), k)
     # bits = itertools.islice(bits,l)
-    return map(lambda bit: reduce(lambda x,y: x*y, bit),bits)
+    return map(lambda bit: reduce(lambda x,y: get(x,y), bit),bits)
+
+def get(x,y):
+    #note: the values are sorted x<y
+    key = "%d,%d" % (x,y)
+    try:
+        return mult[key]
+    except Exception:
+        mu = x*y
+        mult[key] = mu
+        return mu
